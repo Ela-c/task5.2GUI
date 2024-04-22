2ca51fa26fa716300229a67e96527cd1625c1175
Turtleshot <80540625+Ela-c@users.noreply.github.com>
2024-04-22 18:28:21 +1000

    add pwm functionality

--- gui.py
+++ gui.py
@@ -5,12 +5,12 @@ import RPi.GPIO as GPIO
 
 def setup():
     # declare pin standard
-    # GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)
     # Set pin mode for GPIO pins to OUTPUT
     for values in availableColors.values():
         pin = values['pin']
         print("pin: " + str(pin))
-        GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(pin, GPIO.OUT)
 
 
 ## GUI DEFINITIONS ##
@@ -21,15 +21,20 @@ myFont = tkfont.Font(family="Helvetica", size=12, weight="bold")
 ## EVENT FUNCTIONS ##
 
 
-def dimLed(pin: int, duty: int):
+def dimLed(pin, duty, color):
     print(f"button pressed")
     print(f'pin: {pin}')
-    GPIO.PWM(pin, 1000).ChangeDutyCycle(duty)
+    print(f'duty: {duty}')
+    if availableColors[color]['led'] == None:
+        availableColors[color]['led'] = GPIO.PWM(pin, 100)
+        availableColors[color]['led'].start(0)
+
+    availableColors[color]['led'].ChangeDutyCycle(int(duty))
 
 
 def close():
     # turn led off
-    # GPIO.cleanup()
+    GPIO.cleanup()
     print("closing program")
     # exit program
     mainWindow.destroy()
@@ -40,31 +45,35 @@ availableColors = {
         'label': 'Red',
         'hexCode': "#ff0000",
         'value': 'red',
-        'pin': 11,
+        'pin': 19,
+        'led': None,
+
     }, 'blue': {
         'label': 'Blue',
         'hexCode': '#3944bc',
         'value': 'blue',
-        'pin': 10,
+        'pin': 9,
+        'led': None,
     },
     'green': {
         'label': 'Green',
         'hexCode': '#3cb043',
         'value': 'green',
-        'pin': 9,
+        'pin': 10,
+        'led': None,
     },
 
 }
 
 ### WIDGETS ###
 
-redSlider = tk.Scale(mainWindow, from_=0, to=255, orient='horizontal', activebackground='#ffaaaa', bg=availableColors['red']['hexCode'],
-                     bd=0, command=lambda duty: dimLed(availableColors['red']['pin'], duty), font=myFont, label=availableColors['red']['label']).grid()
+redSlider = tk.Scale(mainWindow, from_=0, to=100, orient='horizontal', activebackground='#ffaaaa', bg=availableColors['red']['hexCode'],
+                     bd=0, command=lambda duty: dimLed(availableColors['red']['pin'], duty, 'red'), font=myFont, label=availableColors['red']['label']).grid()
 
-greenSlider = tk.Scale(mainWindow, from_=0, to=255, orient='horizontal', activebackground='#ffaaaa', bg=availableColors['green']['hexCode'],
-                       bd=0, command=lambda duty: dimLed(availableColors['green']['pin'], duty), font=myFont, label=availableColors['red']['label']).grid()
-blueSlider = tk.Scale(mainWindow, from_=0, to=255, orient='horizontal', activebackground='#ffaaaa', bg=availableColors['blue']['hexCode'],
-                      bd=0, command=lambda duty: dimLed(availableColors['blue']['pin'], duty), font=myFont, label=availableColors['red']['label']).grid()
+greenSlider = tk.Scale(mainWindow, from_=0, to=100, orient='horizontal', activebackground='#ffaaaa', bg=availableColors['green']['hexCode'],
+                       bd=0, command=lambda duty: dimLed(availableColors['green']['pin'], duty, 'green'), font=myFont, label=availableColors['red']['label']).grid()
+blueSlider = tk.Scale(mainWindow, from_=0, to=100, orient='horizontal', activebackground='#ffaaaa', bg=availableColors['blue']['hexCode'],
+                      bd=0, command=lambda duty: dimLed(availableColors['blue']['pin'], duty, 'blue'), font=myFont, label=availableColors['red']['label']).grid()
 
 
 # for value in availableColors.values():
